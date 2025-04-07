8fee540cd92e4389857f77e7133f8942e1ebfeae
lcmrl <lcmorelli.eng@gmail.com>
2025-04-07 14:08:51 +0200

    minor changes

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python ./main.py -c ./config/config_euroc.yaml -a ./calibration/calibration_euroc.yaml -w /home/threedom/Desktop/euroc/MH_01_easy/mav0/work_dir/
-#python ./main.py -c ./config/config_carla.yaml -a ./calibration/calibration_carla.yaml -w /home/threedom/Desktop/VAIPOSA/CARLA_DATASET/02_set_with_trajectory/
+python ./main.py -c ./config/config_euroc.yaml -a ./calibration/calibration_euroc.yaml -w /path/to/work_dir/
