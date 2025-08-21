dc13583e1b3b0c63110de638aa464bbb5f152cf0
lcmrl <lcmorelli.eng@gmail.com>
2025-08-21 16:14:51 +0200

    bug inside superpoint implementation

--- src/odometry/local_features.py
+++ src/odometry/local_features.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import time
 import torch
 import numpy as np
 
@@ -17,29 +18,69 @@ class LocalFeatures:
             image_width: int,
             image_height: int,
             config_local_features: dict,
+            verbose: bool = False,
             ) -> None:
+        self.verbose = verbose
         self.feature_name = config_local_features['features_name']
         self.image_width = image_width
         self.image_height = image_height
-        config_sp = config_local_features['superpoint']
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.size ={
+            "height": config_local_features['resize_height'],
+            "width": config_local_features['resize_width'],
+        }
+        self.config_sp = config_local_features['superpoint']
         config_aliked = config_local_features['aliked']
 
         if self.feature_name == "superpoint":
-            self.size ={
-                "height": config_sp['resize_height'],
-                "width": config_sp['resize_width'],
-            }
-            self.processor = AutoImageProcessor.from_pretrained("magic-leap-community/superpoint", do_resize=config_sp['do_resize'], size=self.size)
-            self.model = SuperPointForKeypointDetection.from_pretrained("magic-leap-community/superpoint")
+            self.processor = AutoImageProcessor.from_pretrained("magic-leap-community/superpoint", do_resize=self.config_sp['do_resize'], size=self.size)
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            self.model.to(self.device)
+            if self.device == torch.device("cuda"):
+                self.model = SuperPointForKeypointDetection.from_pretrained("magic-leap-community/superpoint").cuda()
+            else:
+                self.model = SuperPointForKeypointDetection.from_pretrained("magic-leap-community/superpoint")
             self.model.eval()
         
         elif self.feature_name == "aliked":
-            self.device = "cuda" if torch.cuda.is_available() else "cpu"
             self.model = ALIKED(model_name=config_aliked['model_name'], device=self.device, top_k=config_aliked['top_k'], scores_th=config_aliked['scores_th'], n_limit=config_aliked['n_limit'])
+
+    def superpoint(self, img_name: str, image: np.ndarray) -> Tuple[dict, dict]:
+        image = cv2.resize(image, (self.size['width'], self.size['height']))
+        resize_factor = self.size['width'] / self.image_width
+        if self.verbose: t0 = time.time()
+        keypoints = {}
+        descriptors = {}
         
-    def superpoint(self, imgs_dir: Path, image_files: list, batch_size: int) -> Tuple[dict, dict]:
+        with torch.no_grad():
+            images = [image]
+            inputs = self.processor(images, return_tensors="pt").to(self.device)
+            outputs = self.model(**inputs)
+
+            scores = outputs.scores[0]
+            topk = self.config_sp['top_k']
+            if scores.shape[0] < topk:
+                topk = scores.shape[0]
+            topk_indices = torch.topk(scores, topk).indices
+
+            kpts = outputs['keypoints'][0][topk_indices]
+            kpts[:, 0] *= self.image_width / self.size['width']
+            kpts[:, 1] *= self.image_height / self.size['height']
+            keypoints[img_name] = kpts.to("cpu")
+            descriptors[img_name] = outputs['descriptors'][0][topk_indices].to("cpu")
+                
+            del inputs, outputs
+            torch.cuda.empty_cache()
+
+
+        if self.verbose==True:
+            t1 = time.time()
+            print(f"Feature extraction time: {t1-t0:.2f} seconds")
+
+        return keypoints, descriptors
+
+    # Old version of superpoint() working in batches:
+    def superpoint_batch(self, imgs_dir: Path, image_files: list, batch_size: int) -> Tuple[dict, dict]:
+        if self.verbose: t0 = time.time()
         keypoints = {}
         descriptors = {}
         steps = len(image_files) // batch_size
@@ -84,16 +125,27 @@ class LocalFeatures:
                 del inputs, outputs
                 torch.cuda.empty_cache()
 
+        if self.verbose==True:
+            t1 = time.time()
+            print(f"Feature extraction time: {t1-t0:.2f} seconds")
+
         return keypoints, descriptors
 
     def aliked(self, img_name: str, image: np.ndarray) -> Tuple[dict, dict]:
+        image = cv2.resize(image, (self.size['width'], self.size['height']))
+        resize_factor = self.size['width'] / self.image_width
+        if self.verbose: t0 = time.time()
         keypoints = {}
         descriptors = {}
         
         with torch.no_grad():
             pred = self.model.run(image)
-            keypoints[img_name] = torch.from_numpy(pred['keypoints']).to("cpu")
+            keypoints[img_name] = torch.from_numpy(pred['keypoints']).to("cpu")/resize_factor
             descriptors[img_name] = torch.from_numpy(pred['descriptors']).to("cpu")
+
+        if self.verbose==True:
+            t1 = time.time()
+            print(f"Feature extraction time: {t1-t0:.2f} seconds")
 
         return keypoints, descriptors
 
